d79b7f4e1a0b9cec9c4a010f895cb47099690a3d
dharryc <dharrisoncook@gmail.com>
2024-11-12 17:24:40 -0700

    api and static site generator

---
+++ static/Program.cs
@@ -0,0 +1,6 @@
+﻿var layoutHTML = File.ReadAllText("config/layout.html");
+var headerHTML = File.ReadAllText("config/header.html");
+var page1HTML = File.ReadAllText("config/page1-body.html");
+layoutHTML = layoutHTML.Replace("secretstring", headerHTML);
+
+File.WriteAllText("out/index.html", page1HTML);
