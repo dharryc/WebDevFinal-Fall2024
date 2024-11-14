f10befa0c305ae5ece275ef989b5ff4593369021
dharryc <dharrisoncook@gmail.com>
2024-11-14 13:31:26 -0700

    working on static site

--- client/static/Program.cs
+++ client/static/Program.cs
@@ -4,12 +4,42 @@
 // layoutHTML = layoutHTML.Replace("secretstring", headerHTML);
 
 // File.WriteAllText("out/index.html", page1HTML);
+var navTemplate = File.ReadAllText("./config/navbar.html");
+var logInTemplate = File.ReadAllText("./config/login.html");
+string navKey = "NavBar";
+string loginKey = "Login";
+string contentKey = "Content";
 
+//login prototype creation
+var loginPrototype = File.ReadAllText("config/template.html");
+loginPrototype = loginPrototype.Replace(loginKey, logInTemplate);
+loginPrototype = loginPrototype.Replace(navKey, "");
+loginPrototype = loginPrototype.Replace(contentKey, "");
+File.WriteAllText("../src/compiledSite/loginPrototype.html", loginPrototype);
+
+//user creation page Prototype
+var userCreationPrototype = File.ReadAllText("config/template.html");
+
+//list prototype creation (for users to see and edit their own list)
+var userPagePrototype = File.ReadAllText("config/template.html");
+userPagePrototype = userPagePrototype.Replace("pageContent", "<div id=\"form\"> </div>");
+userPagePrototype = userPagePrototype.Replace("Document", "Add Item");
+userPagePrototype = userPagePrototype.Replace("jsSource", "./serviceTest.js");
+File.WriteAllText("../src/compiledSite/userPagePrototype.html", userPagePrototype);
+
+
+//Preview prototype (user will be able to see a "preview" of their page as it will appear to other users)
 var listPrototype = File.ReadAllText("config/template.html");
-listPrototype = listPrototype.Replace("pageContent", "<div id=\"form\"> </div>");
-listPrototype = listPrototype.Replace("Document", "Add Item");
-listPrototype = listPrototype.Replace("jsSource", "./serviceTest.js");
-File.WriteAllText("../src/compiledSite/listprototype.html", listPrototype);
 
+//
+// var listPrototype = File.ReadAllText("config/template.html");
+
+
+// var listPrototype = File.ReadAllText("config/template.html");
+
+
+
+//setting up some basic CSS
 var cssPrototype = File.ReadAllText("config/template-style.css");
 File.WriteAllText("../src/compiledSite/template-style.css", cssPrototype);
+//find out how to run npx tsc in command line when updating
--- client/static/obj/Debug/net8.0/static.AssemblyInfo.cs
+++ client/static/obj/Debug/net8.0/static.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("static")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13fafcb1b662404b7e86e9ac7fced74db65472e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f44b73b1525c621cfe376ab4c2fdb8062f6c39")]
 [assembly: System.Reflection.AssemblyProductAttribute("static")]
 [assembly: System.Reflection.AssemblyTitleAttribute("static")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
