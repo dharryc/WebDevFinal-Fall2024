22f44b73b1525c621cfe376ab4c2fdb8062f6c39
dharryc <dharrisoncook@gmail.com>
2024-11-14 11:17:54 -0700

    working

--- api/Program.cs
+++ api/Program.cs
@@ -11,4 +11,18 @@ app.UseCors(c =>
 
 app.MapGet("/", () => "Hello World!");
 
+var storageRoot = "./storage";
+if (!Directory.Exists(storageRoot)) Directory.CreateDirectory(storageRoot);
+
+app.MapPost("/newUser", async (User user) =>
+{
+  var userPath = Path.Combine(storageRoot, user.UserName);
+  Directory.CreateDirectory(userPath);
+ 
+  var userFile = Path.Combine(userPath, "blog.json");
+  await File.WriteAllTextAsync(userFile, JsonSerializer.Serialize(user));
+});
+
 app.Run();
+
+public record User(string UserName, string Password, string[] Links, string[] Descriptions);
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b8f6a05a1b5be60780e7ab9ed39b2f4b780af1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13fafcb1b662404b7e86e9ac7fced74db65472e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
