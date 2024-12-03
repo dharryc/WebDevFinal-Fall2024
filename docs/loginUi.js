13a4e91d829e55cbf8c90460ede8ca60e4058823
Harry Cook <dharrisoncook@gmail.com>
2024-12-03 12:31:29 -0700

    uh

--- docs/loginUi.js
+++ docs/loginUi.js
@@ -4,7 +4,7 @@ logInNode?.addEventListener("submit", async (ev) => {
     ev.preventDefault();
     const username = document.getElementById("username").value.toLowerCase();
     if (await validateUser(username)) {
-        window.location.href = `userListPagePrototype.html?user=${username}`;
+        window.location.href = `https://dharryc.github.io/WebDevFinal-Fall2024/userListPagePrototype.html?user=${username}`;
     }
     const errorText = document.getElementById("error");
     errorText.textContent = "I couldn't find that user.";
