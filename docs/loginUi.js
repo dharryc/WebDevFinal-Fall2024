6a364569cfbfe15928562ee6ff76258a3dde64b7
Harry Cook <dharrisoncook@gmail.com>
2024-12-03 12:29:48 -0700

    working?

--- docs/loginUi.js
+++ docs/loginUi.js
@@ -4,7 +4,7 @@ logInNode?.addEventListener("submit", async (ev) => {
     ev.preventDefault();
     const username = document.getElementById("username").value.toLowerCase();
     if (await validateUser(username)) {
-        window.location.href = `./userListPagePrototype.html?user=${username}`;
+        window.location.href = `userListPagePrototype.html?user=${username}`;
     }
     const errorText = document.getElementById("error");
     errorText.textContent = "I couldn't find that user.";
