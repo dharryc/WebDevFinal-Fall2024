7eb98c0308a9f76fb3c946cb1ecd806d06eaa0d2
Harry Cook <dharrisoncook@gmail.com>
2024-12-03 12:30:28 -0700

    working?

--- client/src/loginUi.ts
+++ client/src/loginUi.ts
@@ -7,7 +7,7 @@ logInNode?.addEventListener("submit", async (ev) => {
     document.getElementById("username") as any
   ).value.toLowerCase();
   if (await validateUser(username)) {
-    window.location.href = `userListPagePrototype.html?user=${username}`;
+    window.location.href = `https://dharryc.github.io/WebDevFinal-Fall2024/userListPagePrototype.html?user=${username}`;
   }
   const errorText = (document.getElementById("error") as any)
   errorText.textContent = "I couldn't find that user."
